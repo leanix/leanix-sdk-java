0f70c82f57936fa4c9b6d599716d87f92916e08c
Ralf Wehner <ralf.wehner@leanix.net>
2015-10-28 17:42:09 +0100

    checkout jenkins performance plugin with junit result

---
+++ samples/benchmark/src/test/java/FakeTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class FakeTest {
+
+    @Test
+    public void test1() throws InterruptedException {
+        Thread.sleep(1000);
+    }
+
+    @Test
+    public void test2() throws InterruptedException {
+        Thread.sleep(2000);
+    }
+}
