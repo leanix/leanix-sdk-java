948a209f3640b1a10e210f5244a6c9fe0cc16e96
Daniel Pozzi <daniel.pozzi@leanix.net>
2015-10-28 12:16:56 +0100

    [#106599734] excluded compile from time measuring

--- samples/benchmark/benchmark-demo.sh
+++ samples/benchmark/benchmark-demo.sh
@@ -4,8 +4,9 @@ if [[ $# -eq 0 ]] ; then
     exit 0
 fi
 
+mvn compile
 START_TIME=$SECONDS
-mvn compile exec:java -Dexec.mainClass=BenchmarkA -Dapi.basePath=https://local-eam.leanix.net/demo/api/v1 -Dapi.key=55b7b4453a5845ed8370ab3a806e63f7 -Drandom.seed=${1:-0} -Dservices.count=${2:-10} -DresourcesPerServices.count=${3:-5} 
+mvn exec:java -Dexec.mainClass=BenchmarkA -Dapi.basePath=https://local-eam.leanix.net/demo/api/v1 -Dapi.key=55b7b4453a5845ed8370ab3a806e63f7 -Drandom.seed=${1:-0} -Dservices.count=${2:-10} -DresourcesPerServices.count=${3:-5}
 echo $(($SECONDS - $START_TIME))
 echo " seconds elapsed"
 
