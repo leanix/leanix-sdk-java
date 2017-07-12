7847f184af7499099cb712fa7916216c0b7b9a09
Bernd Schoenbach <bernd.schoenbach@leanix.net>
2017-07-12 16:36:16 +0200

    [#146246877] Replaced FilterWrapper with GraphqlQueryIterator and removed unused classes

--- src/main/resources/codegen-templates/net/leanix/api/filter/DateFilter.java
+++ src/main/resources/codegen-templates/net/leanix/api/filter/DateFilter.java
@@ -1,45 +1,68 @@
 package net.leanix.api.filter;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import java.time.LocalDate;
+
 public class DateFilter {
     public enum DateFilterType {
         POINT, RANGE, TODAY, END_OF_MONTH, END_OF_YEAR
     }
 
-    private String minDate;
-    private String maxDate;
-    private String from;
-    private String to;
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private LocalDate minDate;
+
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private LocalDate maxDate;
+
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private LocalDate from;
+
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private LocalDate to;
+
     private DateFilterType type;
 
-    public String getMinDate() {
+    public LocalDate getMinDate() {
         return minDate;
     }
 
-    public void setMinDate(String minDate) {
+    public void setMinDate(LocalDate minDate) {
         this.minDate = minDate;
     }
 
-    public String getMaxDate() {
+    public LocalDate getMaxDate() {
         return maxDate;
     }
 
-    public void setMaxDate(String maxDate) {
+    public void setMaxDate(LocalDate maxDate) {
         this.maxDate = maxDate;
     }
 
-    public String getFrom() {
+    public LocalDate getFrom() {
         return from;
     }
 
-    public void setFrom(String from) {
+    public void setFrom(LocalDate from) {
         this.from = from;
     }
 
-    public String getTo() {
+    public LocalDate getTo() {
         return to;
     }
 
-    public void setTo(String to) {
+    public void setTo(LocalDate to) {
         this.to = to;
     }
 
