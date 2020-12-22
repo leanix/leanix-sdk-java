33a208d20a2b4b018dda326d7ebf2bb7ac69dfc8
Ralf Wehner <ralf.wehner@leanix.net>
2020-12-22 11:27:24 +0100

    regenerated new classes

--- src/main/java/net/leanix/api/SearchbookmarksApi.java
+++ src/main/java/net/leanix/api/SearchbookmarksApi.java
@@ -27,48 +27,14 @@ public class SearchbookmarksApi {
     this.apiClient = apiClient;
   }
 
-    public ApiClient getApiClient() {
-        return apiClient;
-    }
+  public ApiClient getApiClient() {
+    return apiClient;
+  }
 
-    public void setApiClient(ApiClient apiClient) {
-        this.apiClient = apiClient;
-    }
+  public void setApiClient(ApiClient apiClient) {
+    this.apiClient = apiClient;
+  }
 
-<<<<<<< HEAD
-    /**
-     * getBookmarkSuggestions
-     * Returns a list of bookmark suggestions
-     * @param type Bookmark type (optional)
-     * @param subtype Bookmark subtype (optional)
-     * @param q Search term (optional)
-     * @param personalized Personalized (optional, default to false)
-     * @param sort Sorting. Default sorting criterion is bookmark name. (optional)
-     * @param cursor Pagination cursor. The result page will start with the entry immediately after the entry corresponding to the cursor. Order of the cursor values matters.This parameter needs to be specified multiple times: once for each entry in the list, e.g., cursor&#x3D;b2h5by1vaHlv&amp;cursor&#x3D;Ym9va21hcmsj (optional)
-     * @param pageSize Page size (optional, default to 40)
-     * @param sortDirection The direction of the sorting. Default sorting order for name is ASC, for date fields - DSC. (optional)
-     * @param factSheetId FactSheet ID, which must appear in bookmark (applicable only to bookmarks of BookmarkType VISUALIZER) (optional)
-     * @param predefinedOnly Indicates if only predefined bookmarks should be returned. (optional, default to false)
-     * @return BookmarkSuggestionsResponse
-     * @throws ApiException if fails to make API call
-     */
-    public BookmarkSuggestionsResponse getBookmarkSuggestions(
-        String type,
-        String subtype,
-        String q,
-        Boolean personalized,
-        String sort,
-        List<String> cursor,
-        Integer pageSize,
-        String sortDirection,
-        UUID factSheetId,
-        Boolean predefinedOnly) throws ApiException
-    {
-        Object localVarPostBody = null;
-
-        // create path and map variables
-        String localVarPath = "/search/bookmarks".replaceAll("\\{format\\}", "json");
-=======
   /**
    * getBookmarkSuggestions
    * Returns a list of bookmark suggestions
@@ -92,9 +58,8 @@ public class SearchbookmarksApi {
     
     // create path and map variables
     String localVarPath = "/search/bookmarks".replaceAll("\\{format\\}","json");
->>>>>>> release/3.9.30
 
-        // query params
+    // query params
     List<Pair> localVarQueryParams = new ArrayList<Pair>();
     Map<String, String> localVarHeaderParams = new HashMap<String, String>();
     Map<String, Object> localVarFormParams = new HashMap<String, Object>();
